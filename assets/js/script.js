b1c3796e86bda892171793359858644b08990349
BiBax68 <lionel.farias@wanadoo.fr>
2022-02-15 14:24:39 +0100

    ajoute gpu.html + css + js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const btnMenu = document.getElementById('menu');
 const overlayMenu = document.getElementById('overlay_menu');
+var header = document.querySelector("header");
 
 
 btnMenu.addEventListener('click', show);
@@ -8,3 +9,7 @@ function show () {
     btnMenu.classList.toggle("openmenu");
     overlayMenu.classList.toggle('active');
 }
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle('scroll', window.scrollY > 0)
+})
